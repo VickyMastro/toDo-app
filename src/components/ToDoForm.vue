46cbdb24d2a0586c8df1982d396ccf36788976f5
VickyMastro <vicmastroianni1702@gmail.com>
2026-04-15 19:48:01 -0300

    Agrego formulario de tareas

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,18 +1,41 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { useToDosStore } from '../stores/toDosStore.js'
+
+const toDosStore = useToDosStore()
+const description = ref('')
+
+function handleSubmit() {
+  toDosStore.addToDo(description.value)
+  console.log(toDosStore.toDos.length)
+  description.value = ''
+}
+</script>
 
 <template>
-  <form>
-    <input type="text" placeholder="Descripción" />
-    <button type="submit">Agregar</button>
-  </form>
+  <UForm>
+    <UInput
+      v-model="description"
+      type="text"
+      placeholder="Escribe aqui la tarea"
+      :autofocus="false"
+      color="secondary"
+      variant="soft"
+    />
+    <UButton type="submit" label="Agregar" color="secondary" variant="soft" @click="handleSubmit" />
+  </UForm>
 </template>
 
 <style scoped>
 form {
   display: flex;
-  gap: 1rem;
+  gap: 0.5rem;
   justify-content: center;
   align-items: center;
-  margin-top: 1rem;
+  margin: 2rem 0 1rem 0;
+}
+
+button {
+  color: var(--color-warning);
 }
 </style>
